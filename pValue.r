b13cbc918665b4d73f631e3895d4dbbab8df4040
Samuli Ulmanen <sulmanen@thinglink.com>
2016-09-20 18:47:04 +0300

    use dct hamming 4

--- pValue.r
+++ pValue.r
@@ -1,12 +1,12 @@
 
-cm1 <- matrix(c(1, 351, 1694, 1696, 13, 1, 1708, 1368),
+cm1 <- matrix(c(1, 44, 1694, 1605, 13, 98, 1708, 1669),
                 nrow = 2,
                 dimnames = list(c("Local Features", "Simple Hash"), c("FP", "TP", "FN", "TN")))
 addmargins(as.matrix(cm1), c(1,2))
 print("Paintings 53%")
 print(fisher.test(cm1))
 
-cm1 <- matrix(c(1, 350, 1704, 1698, 3, 0, 1708, 1368),
+cm1 <- matrix(c(1, 43, 1704, 1551, 3, 153, 1708, 1669),
                 nrow = 2,
                 dimnames = list(c("Local Features", "Simple Hash"), c("FP", "TP", "FN", "TN")))
 addmargins(as.matrix(cm1), c(1,2))
@@ -15,14 +15,8 @@ print(fisher.test(cm1))
 
 
 
-cm1 <- matrix(c(1, 355, 1702, 1653, 5, 40, 1708, 1368),
-                nrow = 2,
-                dimnames = list(c("Local Features", "Simple Hash"), c("FP", "TP", "FN", "TN")))
-addmargins(as.matrix(cm1), c(1,2))
-print("Paintings Normalize")
-print(fisher.test(cm1))
 
-cm1 <- matrix(c(1, 365, 1705, 1605, 2, 78, 1708, 1368),
+cm1 <- matrix(c(1, 46, 1705, 1667, 2, 34, 1708, 1669),
                 nrow = 2,
                 dimnames = list(c("Local Features", "Simple Hash"), c("FP", "TP", "FN", "TN")))
 addmargins(as.matrix(cm1), c(1,2))
--- tlDctPValue.R
+++ tlDctPValue.R
@@ -1,37 +1,37 @@
 
-cm1 <- matrix(c(34,86, 399, 401, 13, 8, 410, 397),
+cm1 <- matrix(c(34,51, 399, 409, 13, 1, 410, 431),
                 nrow = 2,
-                dimnames = list(c("Local Features", "Simple Hash"), c("FP", "TP", "FN", "TN")))
+                dimnames = list(c("Local Features", "DCT Hash"), c("FP", "TP", "FN", "TN")))
 addmargins(as.matrix(cm1), c(1,2))
 print("ThingLink 53%")
 print(fisher.test(cm1))
 
-cm1 <- matrix(c(35,86, 408, 397, 3, 12, 410, 397),
+cm1 <- matrix(c(35, 48, 408, 409, 3, 1, 410, 431),
                 nrow = 2,
-                dimnames = list(c("Local Features", "Simple Hash"), c("FP", "TP", "FN", "TN")))
+                dimnames = list(c("Local Features", "DCT Hash"), c("FP", "TP", "FN", "TN")))
 addmargins(as.matrix(cm1), c(1,2))
 print("ThingLink 83%")
 print(fisher.test(cm1))
 
-cm1 <- matrix(c(35, 86, 408, 376, 3, 33, 410,  397),
+cm1 <- matrix(c(35, 35, 408, 376, 3, 99, 410,  431),
                 nrow = 2,
-                dimnames = list(c("Local Features", "Simple Hash"), c("FP", "TP", "FN", "TN")))
+                dimnames = list(c("Local Features", "DCT Hash"), c("FP", "TP", "FN", "TN")))
 addmargins(as.matrix(cm1), c(1,2))
 print("Crop 10px")
 print(fisher.test(cm1))
 
 
 
-cm1 <- matrix(c(35, 84, 410, 410, 1, 1,  410, 397),
+cm1 <- matrix(c(35, 50, 410, 407, 1, 4,  410, 431),
                 nrow = 2,
-                dimnames = list(c("Local Features", "Simple Hash"), c("FP", "TP", "FN", "TN")))
+                dimnames = list(c("Local Features", "DCT Hash"), c("FP", "TP", "FN", "TN")))
 addmargins(as.matrix(cm1), c(1,2))
 print("ThingLink Normalize")
 print(fisher.test(cm1))
 
-cm1 <- matrix(c(35,84, 410, 411, 1, 0,  410, 397),
+cm1 <- matrix(c(35,50, 410, 403, 1, 8,  410, 431),
                 nrow = 2,
-                dimnames = list(c("Local Features", "Simple Hash"), c("FP", "TP", "FN", "TN")))
+                dimnames = list(c("Local Features", "DCT Hash"), c("FP", "TP", "FN", "TN")))
 addmargins(as.matrix(cm1), c(1,2))
 print("ThingLink Saturate")
 print(fisher.test(cm1))
